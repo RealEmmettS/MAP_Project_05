77426e752ececa6b7f3faf10aa9978b45d76e25f
Emmett Shaughnessy <github@emmetts.dev>
2021-09-16 10:37:38 -0500

    End of Day 2
    finished tic-tac-toe mechanics. Need to add a way to reset the game and play again.

--- TicTacToe/TicTacToe/UnderTheHoodCode.swift
+++ TicTacToe/TicTacToe/UnderTheHoodCode.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import UIKit
 
 enum Player {
     case x, o
 }
+
 
 struct gameBoard{
     var topLeft         : Player?
@@ -21,4 +23,107 @@ struct gameBoard{
     var bottomLeft      : Player?
     var bottomMiddle    : Player?
     var bottomRight     : Player?
+}
+
+
+
+let winningCombinations = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3] ]
+
+var xArray = [Int]()
+var oArray = [Int]()
+var totalArray = [Int]()
+
+enum Winner{
+    case x, o
+}
+
+
+
+//For the TicTacToe Buttons
+//https://spin.atomicobject.com/2017/07/18/swift-interface-builder/
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    //MARK: Variable / Outlet Setup
     @IBOutlet weak var currentPlayerLabel: UILabel!
+    
+    var ticTacToe:gameBoard = gameBoard()
     
     
     var currentPlayer : Player?{
@@ -33,6 +35,25 @@ class ViewController: UIViewController {
     
     
     
+    var winner : Winner? {
+        didSet{
+            
+            let buttonList : [UIButton] = [topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight]
+            
+            for button in buttonList{
+                if button.isEnabled{
+                    button.isEnabled = false
+                }
+                
+            }//end of for loop
+            
+            currentPlayerLabel.text = "\(winner!)"
+            currentPlayerLabel.textColor = .green
+            
+        }//end of didSet()
+    }//end of winner variable
+    
+    //MARK: viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -52,21 +73,22 @@ class ViewController: UIViewController {
             currentPlayer = .o
         }
         
-        print("Starting Player: \(currentPlayer!)")
+        print("Starting Player: \(currentPlayer!)\n\n")
         
         
         
         let buttonList : [UIButton] = [topLeft, topMiddle, topRight, middleLeft, middleMiddle, middleRight, bottomLeft, bottomMiddle, bottomRight]
-
-        var count = 1
+        
         for button in buttonList{
             button.setTitle("", for: .normal)
-            button.tag = count
-            count += 1
+            
         }
         
     }//end of viewDidLoad()
     
+    
+    
+    //MARK:- nextPlayer()
     func nextPlayer() {
         switch currentPlayer {
         case .x:
@@ -78,13 +100,146 @@ class ViewController: UIViewController {
         }//end of switch
     }//end of nextPlayer()
     
+    //MARK: savePlayerTurn()
+    func savePlayerTurn(placeOnBoard spot: Int){
+        if currentPlayer == .x{
+            xArray.append(spot)
+            totalArray.append(spot)
+            print("X was placed on spot \(spot)")
+        }else{
+            oArray.append(spot)
+            totalArray.append(spot)
+            print("O was placed on spot \(spot)")
+        }
+        
+        print("Total Array updated with play from \(currentPlayer!)\n")
+    }
+    
+    
+    //MARK: checkForWin()
+    /**
+     Checks for a win from  currentPlayer. If there is a win, the game ends. If there is no winner or draw, nextPlayer() is called
+     */
+    func checkForWin(){
+        
+        if currentPlayer == .x{ //runs for player X
+            
+            for combo in winningCombinations {
+                var matches = 0
+                
+                for spot in combo{
+                    if xArray.contains(spot){
+                        matches += 1
+                    }
+                }
+                
+                if matches == 3{
+                    print("\n\nX wins!\n\n")
+                    winner = .x
+                }
+                
+                
+            }//end of root For loop
+            
+        } else { //runs for player O
+            for combo in winningCombinations { 
+                var matches = 0
+                
+                for spot in combo{
+                    if oArray.contains(spot){
+                        matches += 1
+                    }
+                }
+                
+                if matches == 3{
+                    print("\n\nO wins!\n\n")
+                    winner = .o
+                }
+                
+                
+            }//end of root For loop
+        }
+        
+        if winner == nil{
+            nextPlayer()
+        }
+    }//end checkForWin()
     
     
     
+    //MARK:- Button Functions
+    @IBAction func topLeft(_ sender: Any) {
+        ticTacToe.topLeft = currentPlayer
+        topLeft.setTitle("\(currentPlayer!)", for: .normal)
+        topLeft.isEnabled = false
+        savePlayerTurn(placeOnBoard: 1)
+        checkForWin()
+    }
+    
+    @IBAction func topMiddle(_ sender: Any) {
+        ticTacToe.topMiddle = currentPlayer
+        topMiddle.setTitle("\(currentPlayer!)", for: .normal)
+        topMiddle.isEnabled = false
+        savePlayerTurn(placeOnBoard: 2)
+        checkForWin()
+    }
+    
+    @IBAction func topRight(_ sender: Any) {
+        ticTacToe.topRight = currentPlayer
+        topRight.setTitle("\(currentPlayer!)", for: .normal)
+        topRight.isEnabled = false
+        savePlayerTurn(placeOnBoard: 3)
+        checkForWin()
+    }
+    
+    @IBAction func middleLeft(_ sender: Any) {
+        ticTacToe.middleLeft = currentPlayer
+        middleLeft.setTitle("\(currentPlayer!)", for: .normal)
+        middleLeft.isEnabled = false
+        savePlayerTurn(placeOnBoard: 4)
+        checkForWin()
+    }
+    
+    @IBAction func middleMiddle(_ sender: Any) {
+        ticTacToe.middleMiddle = currentPlayer
+        middleMiddle.setTitle("\(currentPlayer!)", for: .normal)
+        middleMiddle.isEnabled = false
+        savePlayerTurn(placeOnBoard: 5)
+        checkForWin()
+    }
+    
+    @IBAction func middleRight(_ sender: Any) {
+        ticTacToe.middleRight = currentPlayer
+        middleRight.setTitle("\(currentPlayer!)", for: .normal)
+        middleRight.isEnabled = false
+        savePlayerTurn(placeOnBoard: 6)
+        checkForWin()
+    }
+    
+    @IBAction func bottomLeft(_ sender: Any) {
+        ticTacToe.bottomLeft = currentPlayer
+        bottomLeft.setTitle("\(currentPlayer!)", for: .normal)
+        bottomLeft.isEnabled = false
+        savePlayerTurn(placeOnBoard: 7)
+        checkForWin()
+    }
+    
+    @IBAction func bottomMiddle(_ sender: Any) {
+        ticTacToe.bottomMiddle = currentPlayer
+        bottomMiddle.setTitle("\(currentPlayer!)", for: .normal)
+        bottomMiddle.isEnabled = false
+        savePlayerTurn(placeOnBoard: 8)
+        checkForWin()
+    }
+    
+    @IBAction func bottomRight(_ sender: Any) {
+        ticTacToe.bottomRight = currentPlayer
+        bottomRight.setTitle("\(currentPlayer!)", for: .normal)
+        bottomRight.isEnabled = false
+        savePlayerTurn(placeOnBoard: 9)
+        checkForWin()
+    }
     
     
-    
-
-
 }
 
